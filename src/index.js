746f0f098f84b053f06dc6234314c50de31cd035
Alex Torres <aletormat@gmail.com>
2018-06-21 16:45:16 +0200

    Add analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './styles/globals';
 
 import App from './containers/App/App';
 
+import ua from 'universal-analytics';
+const visitor = ua('UA-80488337-3');
+
 render(
   <App/>, 
   document.getElementById('root')
